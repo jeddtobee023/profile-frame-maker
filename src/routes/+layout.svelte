9e2b0a2d025f6b46795129b289768edcb1dcf691
Jedd Niñonuevo <jed@jasetech.ca>
2025-03-19 14:39:56 +0800

    Meta Tags Update

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,16 @@
 <script>
 	import "../app.css";
+	import { page } from '$app/stores';
 </script>
 
 <svelte:head>
 	<link href="https://fonts.googleapis.com/css?family=Inter" rel="stylesheet" />
+	<meta property="og:title" content="Profile Frame Maker">
+	<meta property="og:type" content="article" />
+	<meta property="og:url" content={$page.url.href}>
+	<meta property="og:image" content={$page.url.hostname + "/cover-photo.jpg"}>
+	<meta property="og:description" content="Get your Frame for your profile picture!">
+	<meta property="og:site_name" content="Profile Frame Maker">
 </svelte:head>
 
 <main class="bg-slate-800">
